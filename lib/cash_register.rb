8c02234c4f0a80e3b59952e1dda5112112ce42ee
Ryan Mui <reachryanmui@gmail.com>
2019-02-24 02:57:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
     count.times do
       @items << item
     end
-    @last_transaction = (item, price, count)
+    @last_transaction = [item, price, count]
   end
 
   def apply_discount
@@ -25,5 +25,6 @@ class CashRegister
   end
 
   def void_last_transaction
+    @last_transaction
   end
 end
